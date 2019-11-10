4cdd4557d0826620e6a2f2d6b16eafcda31b46cb
Gunasai Garapati <garapatigunasai@gmail.com>
2019-11-10 15:54:17 -0500

    image gallery added.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import styled from "styled-components";
+import { Paper, Fab } from "@material-ui/core";
+import Sushi from "./images/sushi.jpg";
 
 const Container = styled.div`
   display: grid;
@@ -20,19 +22,51 @@ const Header = styled.h1`
   font-size: 2.5rem;
 `;
 
+const ImageHeader = styled.div`
+  padding: 10px;
+`;
+
+const PaperHeader = styled.div`
+  display: grid;
+  grid-gap: 8px;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
 export default class Home extends Component {
   render() {
     const { header, paragraphOne, paragraphTwo } = this.props.data[0];
     return (
-      <Container data-test="Home">
-        <div>
-          <Header>{header && header}</Header>
+      <Fragment>
+        <Container data-test="Home">
+          <div>
+            <Header>{header && header}</Header>
+          </div>
+          <Body>
+            <div>{paragraphOne && paragraphOne}</div>
+            <div>{paragraphTwo && paragraphTwo}</div>
+          </Body>
+        </Container>
+        <div style={{ padding: "0px 10%" }}>
+          <Fab>Test</Fab>
+          <PaperHeader>
+            <Paper>
+              <ImageHeader>
+                <img src={Sushi} alt="sushi" />
+              </ImageHeader>
+            </Paper>
+            <Paper>
+              <ImageHeader>
+                <img src={Sushi} alt="sushi" />
+              </ImageHeader>
+            </Paper>
+            <Paper>
+              <ImageHeader>
+                <img src={Sushi} alt="sushi" />
+              </ImageHeader>
+            </Paper>
+          </PaperHeader>
         </div>
-        <Body>
-          <div>{paragraphOne && paragraphOne}</div>
-          <div>{paragraphTwo && paragraphTwo}</div>
-        </Body>
-      </Container>
+      </Fragment>
     );
   }
 }
